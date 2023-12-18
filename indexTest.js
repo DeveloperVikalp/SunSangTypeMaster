c212ae2efb8e504ed8e0da84dc08d9d4033cd34e
DeveloperVikalp <68001223+DeveloperVikalp@users.noreply.github.com>
2023-12-18 12:59:41 +0530

    Add files via upload

---
+++ indexTest.js
@@ -0,0 +1,93 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Function to get a random key-value pair from the JavaScript object
+    function getRandomKeyValuePair(jsonObj) {
+      const keys = Object.keys(jsonObj);
+      const randomKey = keys[Math.floor(Math.random() * keys.length)];
+      return [randomKey, jsonObj[randomKey]];
+    }
+  
+    // Function to display the result in the specified div
+    function displayRandomKeyValuePair() {
+      fetch("dictionary.json")
+        .then(response => response.json())
+        .then(data => {
+          const divDisplay = document.getElementById("sentence");
+          const [key, value] = getRandomKeyValuePair(data);
+          const resultString = `${key}: ${value}`;
+          divDisplay.textContent = resultString;
+        })
+        .catch(error => console.error("Error fetching JSON:", error));
+    }
+  
+    // Call the function on page load
+    displayRandomKeyValuePair();
+  
+    // Optionally, you can add an event listener to trigger the function on a button click or any other event
+    // For example:
+    // const button = document.getElementById("randomizeButton");
+    // button.addEventListener("click", displayRandomKeyValuePair);
+  });
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 
+// 
+// 
+
+
+
+
+
+
+
+const sentence = "The quick brown fox jumps over the lazy dog.";
+        let currentIndex = 0;
+
+        document.addEventListener('keydown', handleKeyDown);
+
+        function handleKeyDown(event) {
+          const key = event.key;
+          const sentenceElement = document.getElementById('sentence');
+          const currentChar = sentence[currentIndex];
+
+          if (key === currentChar) {
+            sentenceElement.innerHTML = replaceCharAtIndex(sentenceElement.innerText, currentIndex, currentChar, 'green');
+            currentIndex++;
+
+            if (currentIndex === sentence.length) {
+              // Typing completed, do something (e.g., show a success message)
+              alert('Congratulations! You typed the sentence correctly.');
+              resetTyping();
+            }
+          } else {
+            sentenceElement.innerHTML = replaceCharAtIndex(sentenceElement.innerText, currentIndex, currentChar, 'red');
+          }
+        }
+
+        function replaceCharAtIndex(str, index, char, color) {
+          return  `<span class="${"darkGreen"}">${str.substring(0, index)}</span>` + `<span class="${color}">${char}</span>` + str.substring(index + 1);
+        }
+      
+
+        function resetTyping() {
+          currentIndex = 0;
+          displayRandomKeyValuePair();
+        //   const sentenceElement = document.getElementById('sentence');
+        //   sentenceElement.innerHTML = sentence;
+        }
